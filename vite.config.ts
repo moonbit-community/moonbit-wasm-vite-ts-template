e541511c5ca446c8bc71910da4d2e1bccc3b4566
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-03-01 15:05:03 +0800

    fix: vite.config.ts
    use reload option to fresh web page is enough, not need restart vite server
    
    https://github.com/vitejs/vite/discussions/16708#discussioncomment-12346706

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import ViteRestart from 'vite-plugin-restart'
 export default {
   plugins: [
     ViteRestart({
-      restart: [
+      reload: [
         'target/wasm-gc/release/build/mbt/mbt.wasm',
       ]
     })
